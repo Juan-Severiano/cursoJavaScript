ea5d6f779e6bd679571ee2fa662fc0826129e364
Juan <111772146+Juan-Severiano@users.noreply.github.com>
2023-04-21 21:32:30 +0000

    modelo adicionado e criado

---
+++ section03/aula00/index.js
@@ -0,0 +1,12 @@
+/*
+Operadores de Comparação:
+    > maior que
+    >= maoir ou igual a
+    < menor que
+    <= menor ou igual
+    == igual em valor
+    === igual estritamente (valor e tipo)
+    != diferente (valor)
+    !== diferenrte estrito (valor e tipo)
+
+*/
