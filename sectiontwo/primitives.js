dc4fe0e5d6042311291c5b9d06060ba105fa6027
Juan <111772146+Juan-Severiano@users.noreply.github.com>
2023-04-21 19:00:15 +0000

    exercicios

---
+++ sectiontwo/primitives.js
@@ -0,0 +1,16 @@
+/*
+Primitivos (Imutáveis) -
+    string , number , boolean , undefined , null ( bigint, symbol )
+
+Referência (Mutável) -
+    Arrays, Objects, Functions
+ */
+let nome = 'luis'
+nome = "Juan"
+console.log(nome)
+
+let a = 'A'
+let b = a // cópia
+console.log(a, b)
+a = 'shablau'
+console.log(a, b)
