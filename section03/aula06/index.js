5453964e8bd7a10d082a6240bfd99e9c8f8fabdc
Juan <111772146+Juan-Severiano@users.noreply.github.com>
2023-04-22 21:15:45 +0000

    for in finalizado

---
+++ section03/aula06/index.js
@@ -0,0 +1,20 @@
+const pessoas = {
+    nome: 'Juan',
+    sobrenome: 'Severiano',
+    idade: 15
+}
+
+for (let c in pessoas) {
+    console.log(c)
+}
+
+
+/*const frutas = ['Maçã', 'goiaba', 'Pera', 'Uva', 'Banana']
+
+for (let i in frutas) {
+    console.log(frutas[i])
+}
+
+for(let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
+} */
