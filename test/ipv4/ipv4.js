ebde9c1a65c521c02a395e7ea3de09146426c051
juanito <juansoussev@gmail.com>
2023-04-27 19:59:37 -0300

    teste falho

---
+++ test/ipv4/ipv4.js
@@ -0,0 +1,10 @@
+function ipv4NetworkCalculator(ip, mask='', cidr='', rede='', broadcast='', num_ips=0, classe='') {
+    /* return {
+        ip, mask, cidr, rede, broadcast, num_ips, classe,
+        contain_ip() {
+            if ( this.ip == '' ) {
+                throw 'IP não enviado'
+            }
+        }
+    } */
+}
