1f890cc1ce59c750537f4f7d77871494e2307596
Juan <juansoussev@gmail.com>
2023-05-03 11:18:28 -0300

    metade da validação

--- exercicios/ex-validando-form/assets/js/main.js
+++ exercicios/ex-validando-form/assets/js/main.js
@@ -1,4 +1,4 @@
-class ValidacaoForm {
+class ValidaForm {
     constructor() {
         this.form = document.querySelector('.form')
         this.eventos()
@@ -14,11 +14,39 @@ class ValidacaoForm {
     }
     camposValidos() {
         let valid = true
-
-        for (let campo of this.form.querySelector('.validar')) {
-            console.log(campo)
+        for (let errorText of this.form.querySelectorAll('.error-text') ) {
+            errorText.remove()
         }
+
+        for (let campo of this.form.querySelectorAll('.validar')) {
+            const label = campo.previousElementSibling.innerText
+            if (!campo.value) {
+                this.createError(campo, `Campo de ${label}, não pode estar em branco`)
+                valid = false
+            }
+
+            if (campo.classList.contains('cpf')) {
+                if (!this.validaCPF(campo)) valid = false
+            }
+        }
+    }
+    validaCPF(campo) {
+        const cpf = new ValidarCPF(campo.value)
+
+        if(!cpf.validaçao()) {
+            this.createError(campo, 'CPF Inválido')
+            return false
+        }
+
+        return true
+    }
+
+    createError(campo, msg) {
+        const div = document.createElement('div')
+        div.innerHTML = msg
+        div.classList.add('error-text')
+        campo.insertAdjacentElement('afterend', div)
     }
 }
 
-const valida = new ValidacaoForm()
+const valida = new ValidaForm()
