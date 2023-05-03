4bc741c7790a2194d1a17672033fdcd9583e511d
Juan <juansoussev@gmail.com>
2023-05-03 12:57:10 -0300

    validaçao de formulario concluida

--- exercicios/ex-validando-form/assets/js/main.js
+++ exercicios/ex-validando-form/assets/js/main.js
@@ -11,6 +11,30 @@ class ValidaForm {
     handleSubmit(e) {
         e.preventDefault()
         const camposValidos = this.camposValidos()
+        const senhasValidas = this.senhasValidas()
+
+        if (camposValidos && senhasValidas ) {
+            alert('form enviado')
+            this.form.submit()
+        }
+    }
+    senhasValidas() {
+        let valid = true
+
+        const senha = this.form.querySelector('#senha')
+        const rsenha = this.form.querySelector('#rsenha')
+
+        if( senha.value !== rsenha.value ) {
+            valid = false
+            this.createError(senha, 'Campos senha e Repetir senha precisam ser iguais')
+            this.createError(rsenha, 'Campos senha e Repetir senha precisam ser iguais')
+        }
+        if ( senha.length < 6 || senha.length > 12 ) {
+            valid = false 
+            this.createError(senha, ' A senha deve estra entre 6 e 12 caracteres')
+        }
+
+        return valid
     }
     camposValidos() {
         let valid = true
@@ -28,17 +52,39 @@ class ValidaForm {
             if (campo.classList.contains('cpf')) {
                 if (!this.validaCPF(campo)) valid = false
             }
+
+            if (campo.classList.contains('usuario')) {
+                if (!this.validaUser(campo)) valid = false
+            }
         }
+
+        return valid
     }
     validaCPF(campo) {
         const cpf = new ValidarCPF(campo.value)
 
-        if(!cpf.validaçao()) {
+        if(!cpf.valida()) {
             this.createError(campo, 'CPF Inválido')
             return false
         }
 
         return true
+    }
+    validaUser(campo) {
+        const user = campo.value
+        let valid = true
+
+        if (user.length < 3 || user.lentgh > 12 ) {
+            this.createError(campo, 'Usuário precisa ter entre 3 e 12 caracteres')
+            valid = false
+        }
+
+        if ( !user.match(/^[a-zA-Z0-9]+$/g) ) {
+            this.createError(campo, 'Usuário precisa ter apenas letras e numeros')
+            valid = false
+        }
+
+        return valid
     }
 
     createError(campo, msg) {
--- exercicios/ex-validando-form/assets/js/validarCpf.js
+++ exercicios/ex-validando-form/assets/js/validarCpf.js
@@ -2,18 +2,17 @@ class ValidarCPF {
     constructor(cpf) {
         this.cpf = cpf.replace(/\D/g, '')
     }
-    validaçao() {
+    valida() {
         if ( typeof this.cpf === 'undefined' ) return false
         if ( this.cpf.length !== 11 ) return false
-        if (this.isSequencia()) return false
 
         const cpfParcial = this.cpf.slice(0, -2)
         const d1 = this.criaDigito(cpfParcial)
         const d2 = this.criaDigito(cpfParcial + String(d1))
 
         const newCpf = cpfParcial + d1 + d2
-        if(newCpf === this.cpf) return 'Válido'
-        return 'Inválido'
+        if(newCpf === this.cpf) return true
+        return false
     }
     criaDigito(cpfParcial) {
         const arrayCpf = Array.from(cpfParcial)
@@ -27,8 +26,7 @@ class ValidarCPF {
         const digito = 11 - ( total % 11 )
         return digito > 9 ? '0' : String(digito)
     }
-    isSequencia(){
-        const sequencia = this.clearCpf[0].repeat(this.clearCpf.length)
-        return sequencia === this.clearCpf
-    }
 }
+
+// const cpf = new ValidarCPF('075.408.303-95')
+// console.log(cpf.validaçao())
