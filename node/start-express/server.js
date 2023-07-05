4fda9cbfabc4f277f5fe65e48075fcc16b841c5d
juan <juansoussev@gmail.com>
2023-07-05 19:34:10 -0300

    conteudo estatico com a pasta public

--- node/start-express/server.js
+++ node/start-express/server.js
@@ -8,6 +8,7 @@ app.set('view engine', 'ejs')
 
 app.use(routes)
 app.use(express.urlencoded( { extended : true } ))
+app.use(express.static(path.resolve(__dirname, 'public')))
 
 app.listen(3000, () => {
     console.log('Access this link http://127.0.0.1:3000 ')
